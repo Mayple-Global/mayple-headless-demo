ef4c378a715aba265ca0e5b331600c44613029d6
Max Smouha <36583085+squamuglia@users.noreply.github.com>
2025-07-29 18:49:50 -0400

    country again

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,11 @@
 import { headers } from "next/headers";
 
-const getCountry = async () => {
-  const headersList = await headers();
-  return headersList.get("x-vercel-ip-country");
-};
-
 export default async function Home() {
-  const country = await getCountry();
-  console.log({ country });
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-4xl font-bold">Hello World</h1>
-        <p>Country: {country}</p>
+        <p>Country: {(await headers()).get("x-vercel-ip-country")}</p>
       </main>
     </div>
   );
